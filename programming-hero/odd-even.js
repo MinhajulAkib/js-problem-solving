83d4f816034352eaeef8a56823450ed3fe8dfa98
MinhajulAkib <00akibswe@gmail.com>
2021-08-05 20:01:44 +0600

    even-odd number

---
+++ programming-hero/odd-even.js
@@ -0,0 +1,9 @@
+function isEven(number){
+    if (number % 2 == 0){
+        return true;
+    }
+    return false;
+}
+const myNumber = 145;
+var isEvenNumber = isEven (myNumber);
+console.log(isEvenNumber);
