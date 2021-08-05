a293e0ade01c3786f3881d9a5653d60e1d74d2fe
MinhajulAkib <00akibswe@gmail.com>
2021-08-05 09:06:59 +0600

    unit convert

---
+++ unit-convert.js
@@ -0,0 +1,14 @@
+function inchToFeet(inches){
+    var feet = inches/12;
+    return feet;
+}
+var myInches = 132;
+var feet = inchToFeet(myInches);
+console.log('myInches to feet', feet);
+
+var naniInches = 144;
+var feet = inchToFeet(naniInches);
+console.log('Inches to feet', feet);
+
+
+
