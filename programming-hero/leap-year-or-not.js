1f099ac9fe38518219c9f7a3f4116ccfd2f6d6d1
MinhajulAkib <00akibswe@gmail.com>
2021-08-06 22:07:44 +0600

    leap-year-or-not

--- programming-hero/leap-year-or-not.js
+++ programming-hero/leap-year-or-not.js
@@ -21,3 +21,21 @@ console.log(leapyear(2000));
 console.log(leapyear(1700));
 console.log(leapyear(1800));
 console.log(leapyear(100));
+
+
+
+// program to check leap year
+function checkLeapYear(year) {
+
+    //three conditions to find out the leap year
+    if ((0 == year % 4) && (0 != year % 100) || (0 == year % 400)) {
+        console.log(year + ' is a leap year');
+    } else {
+        console.log(year + ' is not a leap year');
+    }
+}
+
+// take input
+const year = prompt('Enter a year:');
+
+checkLeapYear(year);
