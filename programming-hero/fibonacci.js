394bf0062d1e24072c54235040b808a3935d825b
MinhajulAkib <00akibswe@gmail.com>
2021-08-07 19:40:01 +0600

    fibonacci-recursie

--- programming-hero/fibonacci.js
+++ programming-hero/fibonacci.js
@@ -11,21 +11,21 @@ ith = (i-1)th + (i-2)th
 */
  
 
-// const fibo = [0, 1]
-// for (let i =2; i <= 10; i++){
-//     fibo[i] = fibo[i - 1] + fibo[i - 2];
-// }
-// console.log(fibo);
+const fibo = [0, 1]
+for (let i =2; i <= 10; i++){
+    fibo[i] = fibo[i - 1] + fibo[i - 2];
+}
+console.log(fibo);
 
-// function fibonacciSeries(num) {
-//     const fibo = [0, 1];
-//     for (let i = 2; i <= num; i++) {
-//         fibo[i] = fibo[i - 1] + fibo[i - 2];
-//     }
-//     return fibo;
-// }
-// const fiboSeries = fibonacciSeries(30);
-// console.log(fiboSeries);
+function fibonacciSeries(num) {
+    const fibo = [0, 1];
+    for (let i = 2; i <= num; i++) {
+        fibo[i] = fibo[i - 1] + fibo[i - 2];
+    }
+    return fibo;
+}
+const fiboSeries = fibonacciSeries(30);
+console.log(fiboSeries);
 
 
 
