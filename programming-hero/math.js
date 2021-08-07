c558e1c37ae961f7851deb66024690559f01cb6d
MinhajulAkib <00akibswe@gmail.com>
2021-08-07 15:22:27 +0600

    math.js

--- programming-hero/math.js
+++ programming-hero/math.js
@@ -1,4 +1,9 @@
  //absoulate number
- const myNumber = -5;
- const output = Math.abs(myNumber);
+//  const myNumber = -5;
+//  const output = Math.abs(myNumber);
+//  console.log(output);
+
+ //ceil
+ const myNumber = 22.9999;
+ const output = Math.ceil(myNumber);
  console.log(output);
