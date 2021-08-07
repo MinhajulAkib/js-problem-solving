90cdb6d3c774f538627bc9468ebe8a0816279e65
MinhajulAkib <00akibswe@gmail.com>
2021-08-07 15:32:46 +0600

    math.js

--- programming-hero/math.js
+++ programming-hero/math.js
@@ -1,31 +1,31 @@
- //absoulate number
-//  const myNumber = -5;
-//  const output = Math.abs(myNumber);
-//  console.log(output);
+ absoulate number
+ const myNumber = -5;
+ const output = Math.abs(myNumber);
+ console.log(output);
 
- //ceil
-//  const myNumber = 22.9999;
-//  const output = Math.ceil(myNumber);
-//  console.log(output);
+ ceil
+ const myNumber = 22.9999;
+ const output = Math.ceil(myNumber);
+ console.log(output);
 
- //floor
-//  const myNumber = 22.9999;
-//  const output = Math.floor(myNumber);
-//  console.log(output);
-
-
-//round
-//ceil
-// const myNumber = 22.9999;
-// const output = Math.round(myNumber);
-// console.log(output);
+ floor
+ const myNumber = 22.9999;
+ const output = Math.floor(myNumber);
+ console.log(output);
 
 
-//random
-//ceil
-// const myNumber = 7;
-// const output = Math.random(myNumber) * 10;
-// console.log(output);
+round
+ceil
+const myNumber = 22.9999;
+const output = Math.round(myNumber);
+console.log(output);
+
+
+random
+ceil
+const myNumber = 7;
+const output = Math.random(myNumber) * 10;
+console.log(output);
 
 
 for (let i = 0; i <=20; i++){
