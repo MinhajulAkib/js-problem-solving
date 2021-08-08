fe86db9a31180d5b6bf9c9e737942e65bb1f7805
MinhajulAkib <00akibswe@gmail.com>
2021-08-08 21:57:05 +0600

    checklist

---
+++ checklist.js
@@ -0,0 +1,60 @@
+//1. comment
+//single
+/*------*/
+
+//2.variable declaration
+// var number = 10;
+// var name = 'akib';
+// var pass = true/false
+
+//3.let const using 2 numbers sum
+// let a = 10;
+// const b = 12; //value can;t change
+// let c= a + b;
+// let d = a - b;
+// let e = a / b;
+// let f = a % 2;
+// console.log(c);
+// console.log(d);
+// console.log(e);
+// console.log(f);
+
+
+//4.comparison(==,<,>,<=,>=,)
+// var a= 15;
+// var b = 10;
+// if(a == b){
+//     console.log('a is equal to b');
+// }
+// else if(a<b){
+//     console.log('a is less than b');
+// }
+// else if(a>b){
+//     console.log('a is greater than b');
+// }
+// else if(a <= b){
+//     console.log('a is equal or smaller to b')
+// }
+// else if( a>= b){
+//     console.log('a is greater than or equal to b');
+// }
+// else if(a != b){
+//     console.log('a is not equal to b');
+// }
+// else{
+//     console.log('mara kha');
+// }
+
+// var i = 7;
+// while (i < 19){
+//     i = i + 2;
+//     console.log(i);
+// }
+
+
+var mammals = new Array("cat","dog","human","whale","seal");
+var animalString = "";
+for (var i = 0; i < mammals. length; i++) {
+   animalString += mammals[i] + " ";
+}
+console.log(animalString);
