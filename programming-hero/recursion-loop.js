8c29c9197f634219ed276623bd0e2e6303584856
MinhajulAkib <00akibswe@gmail.com>
2021-08-13 11:24:15 +0600

    recursion-for loop

--- programming-hero/recursion-loop.js
+++ programming-hero/recursion-loop.js
@@ -17,8 +17,15 @@
 //     console.log(i);
 // }
 
-let sum = 0;
-for (let i = 6; i >=1; i--){
-    sum = sum +i;
+// let sum = 0;
+// for (let i = 6; i >=1; i--){
+//     sum = sum +i;
+// }
+// console.log(sum);
+function sum(i){
+    if(i == 1){
+        return 1;
+    }
+    return sum(i-1) + i;
 }
-console.log(sum);
+console.log(sum(6));
