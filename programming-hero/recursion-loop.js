f7a16f1155da1234676e3049773a8fb6563187ea
MinhajulAkib <00akibswe@gmail.com>
2021-08-13 11:24:54 +0600

    recursion-for loop

--- programming-hero/recursion-loop.js
+++ programming-hero/recursion-loop.js
@@ -1,27 +1,27 @@
 //1-6 print using for loop
-// for(i = 1; i <=6; i++){
-//     console.log(i);
-// }
+for(i = 1; i <=6; i++){
+    console.log(i);
+}
 
 //sum all input
-// let sum = 0;
-// for(let i = 1; i<= 6; i++){
-//     sum = sum + i;
-// }
-// console.log(sum);
+let sum = 0;
+for(let i = 1; i<= 6; i++){
+    sum = sum + i;
+}
+console.log(sum);
 
 
 //decrement 6 to 1
 
-// for (let i = 6; i >=1; i--){
-//     console.log(i);
-// }
+for (let i = 6; i >=1; i--){
+    console.log(i);
+}
 
-// let sum = 0;
-// for (let i = 6; i >=1; i--){
-//     sum = sum +i;
-// }
-// console.log(sum);
+let sum = 0;
+for (let i = 6; i >=1; i--){
+    sum = sum +i;
+}
+console.log(sum);
 function sum(i){
     if(i == 1){
         return 1;
